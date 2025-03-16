9ce8396959f3cbf43b857899418d49eb8bd5a846
Ayush Sagar <saggar.ayush@gmail.com>
2025-03-16 23:31:31 +0530

    dialog

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,4 +1,15 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Label } from "@/components/ui/label"
 
 function Sidebar() {
   return (
@@ -8,7 +19,36 @@ function Sidebar() {
         <p className='text-2xl font-bold mb-4'>JobB</p>
         </div>
         <div className='p-2'>
-            <button className='bg-indigo-500 p-1 whitespace-nowrap rounded-lg cursor-pointer'>Create new report</button>
+        <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Edit Profile</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+
         </div>
     </div>
     </>
